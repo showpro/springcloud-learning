8740414dc777f1cff99a2f64583ab9324ab7c38f
zhan <42181249@qq.com>
2022-10-26 22:39:19 +0800

    consul注册中心、K/V配置中心、负载均衡的使用

--- consul-config-client/src/main/java/com/macro/cloud/controller/ConfigClientController.java
+++ consul-config-client/src/main/java/com/macro/cloud/controller/ConfigClientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 使用Consul作为配置中心
  * Created by macro on 2019/9/11.
  */
 @RestController
@@ -15,6 +16,15 @@ public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
 
+    /**
+     * 从Consul配置中心中获取配置信息
+     * 在consul中添加配置存储的key为:  config/consul-config-client:dev/data
+     * 在consul中添加配置存储的value为：
+     *  config:
+     *   info: "config info for dev"
+     *
+     * @return
+     */
     @GetMapping("/configInfo")
     public String getConfigInfo() {
         return configInfo;
