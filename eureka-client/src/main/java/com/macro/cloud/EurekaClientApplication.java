80fb2b296e0dd9a2bc682be140f6b8b86c376b88
zhan <42181249@qq.com>
2022-10-26 22:37:52 +0800

    eureka注册中心单机版和集群版 & 通过Actuator优雅的关机服务

--- eureka-client/src/main/java/com/macro/cloud/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/macro/cloud/EurekaClientApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+// @EnableDiscoveryClient//可以不写客户端注解，默认为客户端
 @SpringBootApplication
 public class EurekaClientApplication {
 
--- eureka-security-server/src/main/java/com/macro/cloud/config/WebSecurityConfig.java
+++ eureka-security-server/src/main/java/com/macro/cloud/config/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
+ * 带登录认证的eureka注册中心
+ *
  * 默认情况下添加SpringSecurity依赖的应用每个请求都需要添加CSRF token才能访问，
  * Eureka客户端注册时并不会添加，所以需要配置/eureka/**路径不需要CSRF token。
  * Created by macro on 2019/8/28.
