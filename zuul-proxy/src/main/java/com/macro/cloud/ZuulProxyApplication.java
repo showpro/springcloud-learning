e4dadaba0cc4958925c7ff29c971bbfe96f4e0e7
zhanzhan <zhanzhan@wahaha.com.cn>
2022-10-28 13:20:33 +0800

    Zuul：API网关服务

--- zuul-proxy/src/main/java/com/macro/cloud/ZuulProxyApplication.java
+++ zuul-proxy/src/main/java/com/macro/cloud/ZuulProxyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
+@EnableZuulProxy // 启用Zuul的API网关功能
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulProxyApplication {
