477ed3ca4334d09cf637fc2e41a05e0bc17040c6
zhanzhan <zhanzhan@wahaha.com.cn>
2022-10-31 09:39:03 +0800

    SpringCloud 2020.0.1 版本之后 删除了eureka中的ribbon，替代ribbon的是spring cloud自带的LoadBalancer

--- nacos-ribbon-service/src/main/java/com/macro/cloud/config/RibbonConfig.java
+++ nacos-ribbon-service/src/main/java/com/macro/cloud/config/RibbonConfig.java
@@ -11,6 +11,13 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RibbonConfig {
 
+    /**
+     * 添加了@LoadBalanced注解之后，Ribbon会给restTemplate请求添加一个拦截器，
+     * 在拦截器中获取 注册中心的服务列表，并使用 Ribbon内置的负载均衡算法从服务列表里选中一个服务，默认是轮询策略
+     * 通过获取到的服务信息 （ip,port）替换 serviceId 实现负载请求。
+     *
+     * @return
+     */
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate(){
